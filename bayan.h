e679dc069605eac6b128dea5d1200bbd5a3df279
eugene <lonforton@gmail.com>
2019-08-21 18:43:57 +0300

    Storing hash values in cache instead of files content, fixed public and private areas

--- bayan.h
+++ bayan.h
@@ -27,10 +27,12 @@ public:
     if (algorithm.find("md5") != std::string::npos)
     {
       _algorithm = HashAlgorithm::md_5;      
+      _hash_block_size = 32;
     }
     else
     {
       _algorithm = HashAlgorithm::crc_32;      
+      _hash_block_size = 8;
     }
 
     boost::replace_all(_mask, ".", "\\.");
@@ -39,133 +41,7 @@ public:
     boost::replace_all(_mask, "'?'", ".?");
   }
 
-  std::string get_md5_hash(const std::string &str) const
-  {
-    md5 hash;
-    md5::digest_type digest;
-    hash.process_bytes(str.data(), str.size());
-    hash.get_digest(digest);
-    const auto char_digest = reinterpret_cast<const char *>(&digest);
-    std::string result;
-    boost::algorithm::hex(char_digest, char_digest + sizeof(md5::digest_type), std::back_inserter(result));
-    return result;
-  }
-
-  std::string get_block_hash(const std::string &str) const
-  {
-    if (_algorithm == HashAlgorithm::md_5)
-    {
-      return get_md5_hash(str);
-    }
-    else
-    {
-      boost::crc_32_type result;
-      result.process_bytes(str.data(), str.length());
-      return std::to_string(result.checksum());
-    }
-  }
-
-  bool path_in_results(const std::vector<std::set<std::string>>& vec, const path &file_path)
-  {
-    for (auto const &entry : vec)
-    {
-      if (std::find_if(entry.begin(), entry.end(),
-                       [&file_path](std::string value) { return value == file_path.string(); }) != entry.end())
-      {
-        return true;
-      }
-    }
-    return false;
-  }
-
-  std::string read_from_cache(const path &block_path, const int block_number)
-  {
-    std::string result(_block_size, '\0');
-    auto item_iter = _files_cache.find(block_path.string());
-    if (item_iter != _files_cache.end())
-    {
-      if(item_iter->second.size() > block_number * _block_size)
-      {
-          result = item_iter->second.substr(block_number * _block_size, _block_size);
-      }
-      else
-      {
-         return std::string("");
-      }      
-    }
-    else{
-      return std::string("");
-    }
-
-    return result;
-  }
-
-  void add_to_cache(const path &block_path, const std::string &result)
-  {
-    auto item_iter = _files_cache.find(block_path.string());
-    if (item_iter != _files_cache.end())
-    {
-      (*item_iter).second += result;
-    }
-    else
-    {
-      _files_cache.insert(std::pair<std::string, std::string>(block_path.string(), result));
-    }
-  }
-
-  std::string read_block(const uint32_t block_number, const path &block_path)
-  {
-    std::string cache_result(_block_size, '\0');
-    std::string file_result(_block_size, '\0');
-
-    cache_result = read_from_cache(block_path, block_number);
-    if (!cache_result.empty())
-    {
-      return cache_result;
-    }
-    else
-    {
-      ifstream ifs{block_path};
-      ifs.seekg(block_number * _block_size);
-      if (ifs.peek() == EOF)
-      {
-        return std::string("");
-      }
-      else
-      {
-        ifs.read(&file_result[0], _block_size);
-        add_to_cache(block_path, file_result);
-        return file_result;
-      }
-    }
-
-    return std::string("");
-  }
-
-  bool compare_by_blocks(const path &first_path, const path &second_path)
-  {
-    uint32_t block_number = 0;
-    if(file_size(first_path) != file_size(second_path))
-      return false;
-
-    std::string block_1 = read_block(block_number, first_path);
-    std::string block_2 = read_block(block_number, second_path);
-
-    while (!block_1.empty() && !block_2.empty())
-    {
-      if (get_block_hash(block_1) != get_block_hash(block_2))
-      {
-        return false;
-      }
-      ++block_number;
-      block_1 = read_block(block_number, first_path);
-      block_2 = read_block(block_number, second_path);
-    }
-
-    return true;
-  }
-
-  std::vector<std::set<std::string>> get_duplicate_files()
+    std::vector<std::set<std::string>> get_duplicate_files()
   {
     std::vector<std::set<std::string>> result_vec;
     recursive_directory_iterator dir_it{_dirs};
@@ -179,10 +55,7 @@ public:
         {
           if (
           (!_recursive) 
-          || boost::algorithm::one_of(_exclude_dirs.begin(), _exclude_dirs.end(), 
-                                      [&dir_it](const std::string &exclude_dir) {
-                                         return dir_it->path().filename() == exclude_dir; 
-                                      }) 
+          || boost::algorithm::one_of_equal(_exclude_dirs,  dir_it->path().filename()) 
           )
           {
             dir_it.no_push();            
@@ -190,7 +63,8 @@ public:
           ++dir_it;
           continue;
         }
-        else if( (file_size(dir_it->path()) < _size)
+        else if( !is_regular_file(status(*dir_it))
+        || (file_size(dir_it->path()) < _size)
         ||  !boost::regex_match(dir_it->path().filename().string(), what, my_filter))
         {       
           ++dir_it;
@@ -228,6 +102,136 @@ public:
   }
 
 private:
+
+  std::string get_md5_hash(const std::string &str) const
+  {
+    md5 hash;
+    md5::digest_type digest;
+    hash.process_bytes(str.data(), str.size());
+    hash.get_digest(digest);
+    const auto char_digest = reinterpret_cast<const char *>(&digest);
+    std::string result;
+    boost::algorithm::hex(char_digest, char_digest + sizeof(md5::digest_type), std::back_inserter(result));
+    return result;
+  }
+
+  std::string get_block_hash(const std::string &str) const
+  {
+    if (_algorithm == HashAlgorithm::md_5)
+    {
+      return get_md5_hash(str);
+    }
+    else
+    {
+      boost::crc_32_type result;
+      result.process_bytes(str.data(), str.length());
+      std::stringstream stream;
+      stream << std::hex << result.checksum();
+      return stream.str();
+    }
+  }
+
+  bool path_in_results(const std::vector<std::set<std::string>>& vec, const path &file_path)
+  {
+    for (auto const &entry : vec)
+    {
+      if (std::find_if(entry.begin(), entry.end(),
+                       [&file_path](const std::string &value) { return value == file_path.string(); }) != entry.end())
+      {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  std::string read_from_cache(const path &block_path, const int block_number)
+  {
+    std::string result(_hash_block_size, '\0');
+    auto item_iter = _files_cache.find(block_path.string());
+    if (item_iter != _files_cache.end())
+    {
+      if(item_iter->second.size() > block_number * _hash_block_size)
+      {
+          return item_iter->second.substr(block_number * _hash_block_size, _hash_block_size);
+      }
+      else
+      {
+         return std::string();
+      }      
+    }
+    else{
+      return std::string();
+    }
+
+    return std::string();
+  }
+
+  void add_to_cache(const path &block_path, const std::string &result)
+  {
+    auto item_iter = _files_cache.find(block_path.string());
+    if (item_iter != _files_cache.end())
+    {
+      (*item_iter).second += result;
+    }
+    else
+    {
+      _files_cache.insert(std::pair<std::string, std::string>(block_path.string(), result));
+    }
+  }
+
+  std::string read_block(const uint32_t block_number, const path &block_path)
+  {
+    std::string cache_result(_block_size, '\0');
+    std::string file_result(_block_size, '\0');
+
+    cache_result = read_from_cache(block_path, block_number);
+    if (!cache_result.empty())
+    {
+      return cache_result;
+    }
+    else
+    {
+      ifstream ifs{block_path};
+      ifs.seekg(block_number * _block_size);
+      if (ifs.peek() == EOF)
+      {
+        return std::string("");
+      }
+      else
+      {
+        ifs.read(&file_result[0], _block_size);
+        std::string hash_result = get_block_hash(file_result); 
+        add_to_cache(block_path, hash_result);
+        return hash_result;
+      }
+    }
+
+    return std::string("");
+  }
+
+  bool compare_by_blocks(const path &first_path, const path &second_path)
+  {
+    uint32_t block_number = 0;
+    if(file_size(first_path) != file_size(second_path))
+      return false;
+
+    std::string block_1 = read_block(block_number, first_path);
+    std::string block_2 = read_block(block_number, second_path);
+
+    while (!block_1.empty() && !block_2.empty())
+    {
+      if (block_1 != block_2)
+      {
+        return false;
+      }
+      ++block_number;
+      block_1 = read_block(block_number, first_path);
+      block_2 = read_block(block_number, second_path);
+    }
+
+    return true;
+  }
+
   std::string _dirs;
   std::vector<std::string> _exclude_dirs;
   bool _recursive;
@@ -236,4 +240,5 @@ private:
   uint64_t _block_size;
   HashAlgorithm _algorithm;
   std::map<std::string, std::string> _files_cache;
+  uint64_t _hash_block_size;
 };
