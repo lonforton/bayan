ba9913b5a719cccb7a80c126608986d5110e7dda
eugene <lonforton@gmail.com>
2019-08-22 13:06:40 +0300

    Right slashes for testing on travis

--- unit_tests.cpp
+++ unit_tests.cpp
@@ -72,11 +72,6 @@ private:
 
 BOOST_AUTO_TEST_SUITE(bayan_test_suite)
 
-BOOST_FIXTURE_TEST_CASE(small_test, BayanFixtureTest)
-{
-   BOOST_CHECK(1 == 1);
-}
-
 
 BOOST_FIXTURE_TEST_CASE(bayan_input_test, BayanFixtureTest)
 {
@@ -98,62 +93,62 @@ BOOST_FIXTURE_TEST_CASE(bayan_input_test, BayanFixtureTest)
   }
 
   BOOST_CHECK(output.is_equal("\n\
-test_root\\test_A\\test_A_1\\C_1\n\
-test_root\\test_B\\test_B_1\\C_1\n\
+test_root/test_A/test_A_1/C_1\n\
+test_root/test_B/test_B_1/C_1\n\
 \n\
-test_root\\test_A\\test_A_1\\C_2\n\
-test_root\\test_B\\test_B_1\\C_2\n\
+test_root/test_A/test_A_1/C_2\n\
+test_root/test_B/test_B_1/C_2\n\
 \n\
-test_root\\test_A\\test_A_2\\C_4\n\
-test_root\\test_B\\test_B_2\\C_4\n"));
+test_root/test_A/test_A_2/C_4\n\
+test_root/test_B/test_B_2/C_4\n"));
 }
 
-// BOOST_FIXTURE_TEST_CASE(bayan_exclude_test_1, BayanFixtureTest)
-// {
-//   std::vector<std::string> excludes;
-//   excludes.push_back("test_A");
-//   Bayan bayan("test_root", excludes, true, 1, "'*'", 5, "crc32");  
+BOOST_FIXTURE_TEST_CASE(bayan_exclude_test_1, BayanFixtureTest)
+{
+  std::vector<std::string> excludes;
+  excludes.push_back("test_A");
+  Bayan bayan("test_root", excludes, true, 1, "'*'", 5, "crc32");  
   
-//   boost::test_tools::output_test_stream output;
-//   {
-//     cout_redirect redirect(output.rdbuf());
-//     auto duplicate_files = bayan.get_duplicate_files();
-//     for (auto itr : duplicate_files)
-//     {
-//       std::cout << std::endl;
-//       for (const auto &se : itr)
-//       {
-//         std::cout << se << std::endl;
-//       }
-//     }
-//   }
+  boost::test_tools::output_test_stream output;
+  {
+    cout_redirect redirect(output.rdbuf());
+    auto duplicate_files = bayan.get_duplicate_files();
+    for (auto itr : duplicate_files)
+    {
+      std::cout << std::endl;
+      for (const auto &se : itr)
+      {
+        std::cout << se << std::endl;
+      }
+    }
+  }
 
-//   BOOST_CHECK(output.is_equal(""));
-// }
+  BOOST_CHECK(output.is_equal(""));
+}
 
-// BOOST_FIXTURE_TEST_CASE(bayan_exclude_test_2, BayanFixtureTest)
-// {
-//   std::vector<std::string> excludes;
-//   excludes.push_back("test_A_1");
-//   Bayan bayan("test_root", excludes, true, 1, "'*'", 5, "crc32");  
+BOOST_FIXTURE_TEST_CASE(bayan_exclude_test_2, BayanFixtureTest)
+{
+  std::vector<std::string> excludes;
+  excludes.push_back("test_A_1");
+  Bayan bayan("test_root", excludes, true, 1, "'*'", 5, "crc32");  
   
-//   boost::test_tools::output_test_stream output;
-//   {
-//     cout_redirect redirect(output.rdbuf());
-//     auto duplicate_files = bayan.get_duplicate_files();
-//     for (auto itr : duplicate_files)
-//     {
-//       std::cout << std::endl;
-//       for (const auto &se : itr)
-//       {
-//         std::cout << se << std::endl;
-//       }
-//     }
-//   }
+  boost::test_tools::output_test_stream output;
+  {
+    cout_redirect redirect(output.rdbuf());
+    auto duplicate_files = bayan.get_duplicate_files();
+    for (auto itr : duplicate_files)
+    {
+      std::cout << std::endl;
+      for (const auto &se : itr)
+      {
+        std::cout << se << std::endl;
+      }
+    }
+  }
 
-//   BOOST_CHECK(output.is_equal("\n\
-// test_root\\test_A\\test_A_2\\C_4\n\
-// test_root\\test_B\\test_B_2\\C_4\n"));
-// }
+  BOOST_CHECK(output.is_equal("\n\
+test_root/test_A/test_A_2/C_4\n\
+test_root/test_B/test_B_2/C_4\n"));
+}
 
 BOOST_AUTO_TEST_SUITE_END()
